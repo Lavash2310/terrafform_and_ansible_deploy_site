7daa8ff20174d8c463cf12704ca908e110240bf9
Maksym Romanyuk <maksym23102006@gmail.com>
2026-01-05 12:46:20 +0200

    feat: Terraform fmt in compute.tf

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -9,8 +9,8 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name   = "aws_project_key"
-# public_key = file(pathexpand("${var.ssh_key_path}.pub")) 
+  key_name = "aws_project_key"
+  # public_key = file(pathexpand("${var.ssh_key_path}.pub")) 
   public_key = var.ssh_public_key
 }
 
